8e342de840046be83292910f83bc4c2b7643c17d
saikumar <wwwsaikumar150@gmail.com>
2025-12-08 17:38:45 +0530

    functions

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -49,7 +49,7 @@ mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "kubectl installation"
 
 # Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
 chmod 700 get_helm.sh
 ./get_helm.sh
 VALIDATE $? "helm installation"
